77cd700caedd1748afdfc0816317453f802ae846
Eduardo Marçal <edumsfilho347@gmail.com>
2023-11-21 10:21:10 -0300

    fix: connection to postgres container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ApiModule],
+  imports: [
+    ConfigModule.forRoot({ ignoreEnvFile: false, isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      url: process.env.DB_URL,
+      entities: [process.env.ENTITY_PATH],
+      synchronize: true,
+    }),
+    ApiModule,
+  ],
 })
 export class AppModule {}
