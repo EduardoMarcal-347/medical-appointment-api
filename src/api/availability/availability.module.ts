6e2be22868c817e140ce6c5480452afd1499a1c8
Eduardo Marçal <edumsfilho347@gmail.com>
2023-12-04 18:09:01 -0300

    fix: adding doctor repository at imports

--- src/api/availability/availability.module.ts
+++ src/api/availability/availability.module.ts
@@ -3,9 +3,10 @@ import { AvailabilityService } from './availability.service';
 import { AvailabilityController } from './availability.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AvailabilityEntity } from './availability.entity';
+import { DoctorEntity } from '../doctor/doctor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AvailabilityEntity])],
+  imports: [TypeOrmModule.forFeature([AvailabilityEntity, DoctorEntity])],
   controllers: [AvailabilityController],
   providers: [AvailabilityService],
 })
