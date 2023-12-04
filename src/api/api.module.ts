0be6cc65d9bd70c04c668b61c78e7ad7bf0c838d
Eduardo Marçal <edumsfilho347@gmail.com>
2023-12-04 17:57:41 -0300

    refactor: adding availability module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DoctorModule } from './doctor/doctor.module';
+import { AvailabilityModule } from './availability/availability.module';
 
 @Module({
-  imports: [UserModule, DoctorModule],
+  imports: [UserModule, DoctorModule, AvailabilityModule],
 })
 export class ApiModule {}
