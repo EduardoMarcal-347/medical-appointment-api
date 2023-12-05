abfa291207980e79aa88755f7e231b8ce79b6384
Eduardo Marçal <edumsfilho347@gmail.com>
2023-12-05 00:11:53 -0300

    refactor: renaming variables and endpoints

--- src/api/availability/availability.controller.ts
+++ src/api/availability/availability.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Delete,
   Put,
+  UseFilters,
 } from '@nestjs/common';
 import { AvailabilityService } from './availability.service';
 import { CreateAvailabilityDto } from './dto/create-availability.dto';
@@ -13,7 +14,9 @@ import { UpdateAvailabilityDto } from './dto/update-availability.dto';
 import { ViewAvailabilityDto } from './dto/view-availability.dto';
 import { ApiOkResponsePaginated } from 'src/swagger/apiOkResponsePaginated';
 import { DeleteResult } from 'typeorm';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
 
+@UseFilters(HttpExceptionFilter)
 @Controller('api/v1/availability')
 export class AvailabilityController {
   constructor(private readonly availabilityService: AvailabilityService) {}
@@ -21,36 +24,36 @@ export class AvailabilityController {
   @Post()
   @ApiOkResponsePaginated(ViewAvailabilityDto)
   async create(
-    @Body() createAvailabilityDto: CreateAvailabilityDto,
+    @Body() reqBody: CreateAvailabilityDto,
   ): Promise<ViewAvailabilityDto> {
-    return this.availabilityService.create(createAvailabilityDto);
+    return this.availabilityService.create(reqBody);
   }
 
-  @Get(':id')
+  @Get('doctor/id/:id')
   @ApiOkResponsePaginated(ViewAvailabilityDto)
   async findAllByDoctor(
     @Param('id') reqId: number,
   ): Promise<ViewAvailabilityDto[]> {
-    return this.availabilityService.findAllByDoctor(+reqId);
+    return await this.availabilityService.findAllByDoctor(reqId);
   }
 
-  @Get(':id')
+  @Get('id/:id')
   @ApiOkResponsePaginated(ViewAvailabilityDto)
   async findById(@Param('id') reqId: number): Promise<ViewAvailabilityDto> {
-    return this.availabilityService.findById(+reqId);
+    return this.availabilityService.findById(reqId);
   }
 
-  @Put(':id')
+  @Put('id/:id')
   @ApiOkResponsePaginated(ViewAvailabilityDto)
   async update(
     @Param('id') reqId: number,
-    @Body() updateAvailabilityDto: UpdateAvailabilityDto,
+    @Body() reqBody: UpdateAvailabilityDto,
   ): Promise<ViewAvailabilityDto> {
-    return this.availabilityService.update(+reqId, updateAvailabilityDto);
+    return this.availabilityService.update(reqId, reqBody);
   }
 
-  @Delete(':id')
+  @Delete('id/:id')
   async delete(@Param('id') reqId: number): Promise<DeleteResult> {
-    return this.availabilityService.delete(+reqId);
+    return this.availabilityService.delete(reqId);
   }
 }
