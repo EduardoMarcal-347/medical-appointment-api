d34cbe61d663175df15ca689db5bf761470004b9
Eduardo Marçal <edumsfilho347@gmail.com>
2023-12-05 00:15:04 -0300

    refactor: removing attributes from omitted types

--- src/api/user/dto/view-user.dto.ts
+++ src/api/user/dto/view-user.dto.ts
@@ -2,16 +2,14 @@ import { OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { UserEntity } from '../user.entity';
 
-export class ViewUserDto extends OmitType(CreateUserDto, [
-  'cpf',
-  'phoneNumber',
-  'address',
-]) {
+export class ViewUserDto extends OmitType(CreateUserDto, ['cpf']) {
   constructor(entity: UserEntity) {
     super();
     this.name = entity.name;
     this.birthdate = entity.birthdate;
     this.healthInsurance = entity.healthInsurance;
     this.email = entity.email;
+    this.phoneNumber = entity.phoneNumber;
+    this.address = entity.address;
   }
 }
