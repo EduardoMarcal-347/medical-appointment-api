9c8a97475d0d40cb170541fadac915b6fe23cdc1
Eduardo Marçal <edumsfilho347@gmail.com>
2023-12-11 04:25:08 -0300

    refactor: adding appointment module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { AvailabilityModule } from './availability/availability.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [UserModule, DoctorModule, AvailabilityModule],
+  imports: [UserModule, DoctorModule, AvailabilityModule, AppointmentModule],
 })
 export class ApiModule {}
